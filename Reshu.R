3ab9f58355c55b555b57682ffa63c5f6c1a8a9d1
reshma252004july-cell <reshma252004july@gmail.com>
2025-09-08 11:17:56 +0530

    Initial commit

---
+++ Reshu.R
@@ -0,0 +1,19 @@
+#Loading Libraie
+library(arules)
+library(arulesViz)
+library(RColorBrewer)
+#import dataset
+data("Groceries")
+#using apriori()function
+rules<-apriori(Groceries,
+               parameter=list(supp=0.01,conf=0.2))
+#using inspect()function
+inspect(rules[1:10])
+#using itemFrequencyPlot()function 
+itemFrequencyPlot(Groceries,
+                  topN=20,
+                  col=brewer.pal(8,'pastel2'),
+                  main='Relative Item Frequency Plot',
+                  type="relative",
+                  ylab="Item Frequency Plot(Relative)")
+
